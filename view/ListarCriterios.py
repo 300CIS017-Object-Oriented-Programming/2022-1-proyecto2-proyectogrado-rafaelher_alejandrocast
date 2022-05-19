5ea568b3737f3380eb4aba194cb1631d693663f5
rafaaga <rafaelhermida@javerianacali.edu.co>
2022-05-19 18:33:55 -0500

    Update ListarCriterios.py

--- view/ListarCriterios.py
+++ view/ListarCriterios.py
@@ -8,50 +8,18 @@ def search(clave, d):
 
 
 def Listar_Criterios(st,controller):
-    ids = []
-    nombres = controller.nombres
+    l = []
     st.title("LISTA DE CRITERIOS ")
-    st.subheader("Selecciona el ID del estudiante a listar")
-    for calificacion in controller.evaluaciones:
-        ids.append(calificacion.id_estudiante)
-    seleccion = st.selectbox("Seleccione:", ids)
-
-    """Itera los elementos de evaluacion agregados y los muestra"""
-    i = 1
-    index = search(seleccion, nombres)
+    st.subheader("Selecciona el nombre del bloque de criterios a listar")
+    for key in controller.criterios:
+        l.append(key)
+    seleccion = st.selectbox("Seleccione:", l)
+    index = search(seleccion, l)
     contador = 0
-    for evaluacion in controller.evaluaciones:
+    for key in controller.criterios:
         if (contador == index):
-            st.title("ACTA NÚMERO "+str(i))
-
-            st.subheader("Nombre")
-            st.write(evaluacion.nombre)
-       # with col1:
-            st.subheader("ID Estudiante")
-            st.write(evaluacion.id_estudiante)
-        #with col1:
-            st.subheader("Tema del proyecto")
-            st.write(evaluacion.tema_proyecto)
-        #with col1:
-            st.subheader("Periodo")
-            st.write(evaluacion.periodo)
-        #with col1:
-            st.subheader("Director")
-            st.write(evaluacion.director)
-        #with col1:
-            st.subheader("Co-Director")
-            st.write(evaluacion.co_director)
-        #with col1:
-            st.subheader("Énfasis")
-            st.write(evaluacion.enfasis)
-        #with col1:
-            st.subheader("Modalidad")
-            st.write(evaluacion.modalidad)
-        #with col1:
-            st.subheader("Jurado 1")
-            st.write(evaluacion.jurado1)
-        #with col1:
-            st.subheader("Jurado 2")
-            st.write(evaluacion.jurado2)
-        i += 1
+            st.title("Bloque "+key)
+            aux = controller.criterios.get(key)
+            for key2 in aux:
+                st.subheader(key2+ " - "+str(aux.get(key2))+" %")
         contador += 1
