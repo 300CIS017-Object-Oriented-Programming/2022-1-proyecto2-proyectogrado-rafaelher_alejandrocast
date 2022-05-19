e1c4fd3df48d98f4154c2a80a5c9ac53fd49dbab
rafaaga <rafaelhermida@javerianacali.edu.co>
2022-05-18 22:02:11 -0500

    Implementación multicriterio
    (Beta)

--- controller/EvalController.py
+++ controller/EvalController.py
@@ -5,6 +5,14 @@ class EvaluadorController:
         self.evaluaciones = []
         self.nombres = {}
         self.calificaciones = {}
+        self.criterios = {"Actuales": {"Desarrollo y profundidad en el tratamiento del tema": 20,
+                                       "Desafío académico y científico del tema": 15,
+                                       "Cumplimiento de los objetivos propuestos": 10,
+                                       "Creatividad e innovación de las soluciones y desarrollos propuestos": 10,
+                                       "Validez de los resultados y conclusiones": 20,
+                                       "Manejo y procesamiento de la información y bibliografía": 10,
+                                       "Calidad y presentación del documento escrito": 7.5,
+                                       "Presentación oral": 7.5} }
 
     def agregar_evaluacion(self, evaluacion_obj):
         self.evaluaciones.append(evaluacion_obj)
--- view/CalificarActa.py
+++ view/CalificarActa.py
@@ -1,9 +1,30 @@
 import streamlit as st
+def search(index, d):
+   i = 0
+   for key in d:
+      if i == index:
+         return key
+      i+=1
+
+def calificar(i, crt_actual, controller, seleccion, verificar):
+    actual = search(i, crt_actual)
+    nota_crt = st.number_input(actual, min_value=0.0, max_value=5.0, step=0.1)
+    st.text("Observaciones:")
+    observacion = st.text_input()
+    guardar = st.button("Guardar")
+    if guardar:
+        notas = []
+        tupla = (nota_crt, observacion)
+        notas.append(tupla)
+        i += 1
+        verificar += 1
+        st.text("Se ha calificado a " + str(controller.nombres.get(seleccion)) + " exitosamente")
 
 def calificar_acta(st, controller):
 
     ids = []
     nombres = controller.nombres
+    criterios = controller.criterios
     st.title("CALIFICACIÓN DE ACTAS ")
     st.subheader("Selecciona el ID del estudiante a calificar")
     for calificacion in controller.evaluaciones:
@@ -11,37 +32,15 @@ def calificar_acta(st, controller):
     seleccion = st.selectbox("Seleccione:", ids)
     if (seleccion != None):
         st.text("Estás calificando a "+str(nombres.get(seleccion)))
-        st.subheader("Criterios a calificar")
-        criterio_1 = st.number_input("Desarrollo y profundidad en el tratamiento del tema:",min_value=0.0, max_value=5.0, step=0.1)
-        observacion_1 = st.text_input("Observaciones:")
-        criterio_2 = st.number_input("Desafío académico y científico del tema:",min_value=0.0, max_value=5.0, step=0.1)
-        observacion_2 = st.text_input("Observaciones: ")
-        criterio_3 = st.number_input("Cumplimiento de los objetivos propuestos:",min_value=0.0, max_value=5.0, step=0.1)
-        observacion_3 = st.text_input("Observaciones:  ")
-        criterio_4 = st.number_input("Creatividad e innovación de las soluciones y desarrollos propuestos:",min_value=0.0, max_value=5.0, step=0.1)
-        observacion_4 = st.text_input("Observaciones:   ")
-        criterio_5 = st.number_input("Validez de los resultados y conclusiones:",min_value=0.0, max_value=5.0, step=0.1)
-        observacion_5 = st.text_input("Observaciones:    ")
-        criterio_6 = st.number_input("Manejo y procesamiento de la información y bibliografía:",min_value=0.0, max_value=5.0, step=0.1)
-        observacion_6 = st.text_input("Observaciones:     ")
-        criterio_7 = st.number_input("Calidad y presentación del documento escrito:",min_value=0.0, max_value=5.0, step=0.1)
-        observacion_7 = st.text_input("Observaciones:      ")
-        guardar = st.button("Guardar")
-        if guardar:
-            notas = []
-            t_1 = (criterio_1, observacion_1)
-            notas.append(t_1)
-            t_2 = (criterio_2, observacion_2)
-            notas.append(t_2)
-            t_3 = (criterio_3, observacion_3)
-            notas.append(t_3)
-            t_4 = (criterio_4, observacion_4)
-            notas.append(t_4)
-            t_5 = (criterio_5, observacion_5)
-            notas.append(t_5)
-            t_6 = (criterio_6, observacion_6)
-            notas.append(t_6)
-            t_7 = (criterio_7, observacion_7)
-            notas.append(t_7)
-            controller.calificaciones[seleccion] = notas
-            st.text("Se ha calificado a "+str(nombres.nombres.get(seleccion))+" exitosamente")
+        st.subheader("Seleccione bloque criterios a calificar")
+        seleccion_2 = st.selectbox("Selección:", criterios.keys())
+        if (seleccion != None):
+            crt_actual = criterios.get(seleccion_2)
+            i = 0
+            st.subheader("Establezca la nota y deje sus comentarios")
+            verificar = 0
+            while (i<len(crt_actual)):
+                if (verificar == 0):
+                    calificar(i, crt_actual, controller, seleccion, verificar)
+                verificar += 1
+                #controller.calificaciones[seleccion] = notas
